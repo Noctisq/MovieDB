b5a60c9989950af5fb37f14b065f4425ea79fad5
Noctisq <noctisumire@gmail.com>
2021-03-05 17:02:06 -0600

    Added Styles and an extra feature

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { setQuery } from '../redux/query';
+const getStorageTheme = () => {
+  let theme = 'light-theme';
+  if (localStorage.getItem('theme')) {
+    theme = localStorage.getItem('theme');
+  }
+  return theme;
+};
 
 const SearchForm = () => {
-  
+
   const dispatch = useDispatch();
   const { query } = useSelector((state) => state.query);
   const { error } = useSelector((state) => state.error);
+  const [theme, setTheme] = useState(getStorageTheme());
+
+  const toggleTheme = () => {
+    if (theme === 'light-theme') {
+      setTheme('dark-theme');
+    } else {
+      setTheme('light-theme');
+    }
+  };
+
+  useEffect(() => {
+    document.documentElement.className = theme;
+    localStorage.setItem('theme', theme);
+  }, [theme]);
 
   return (<form className="search-form" onSubmit={(e) => {
     e.preventDefault();
   }}>
-    <h2>Search Movies</h2>
+    <nav>
+      <div className="nav-center">
+        <h1>Search Movies</h1>
+        <button onClick={toggleTheme} className="btn" >
+          surprise :o
+          </button>
+      </div>
+    </nav>
+
+
     <input type="text" className="form-input" value={query}
       onChange={(e) => {
         dispatch(setQuery(e.target.value));
       }} />
+
     {error && < div className="error">No results found :(</div>}
   </form>)
 }
