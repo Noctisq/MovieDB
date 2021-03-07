791fe4bd3db6660ad88eb598ab110d62c58fa029
Noctisq <noctisumire@gmail.com>
2021-03-06 21:34:11 -0600

    Sorts, styles and more Features Added

--- src/redux/movies.js
+++ src/redux/movies.js
@@ -11,17 +11,35 @@ export const moviesSlice = createSlice({
 
         setIsLoading: (state) => {
 
-            
+
             state.isLoading = !state.isLoading
         },
 
         setMovies: (state, action) => {
-            
+
             state.moviesResults = action.payload
+        },
+
+        sortMoviesByName: (state) => {
+            state.moviesResults = state.moviesResults.sort((first, second) => {
+                if (first.title < second.title) { return -1; }
+                if (first.title > second.title) { return 1; }
+                return 0;
+            });
+        },
+
+        sortMoviesByScore: (state) => {
+            state.moviesResults = state.moviesResults.sort((first, second) => {
+                if (first.vote_average < second.vote_average) { return 1; }
+                if (first.vote_average > second.vote_average) { return -1; }
+                return 0;
+            });
         }
+
+
     }
 })
 
-export const { setIsLoading, setMovies } = moviesSlice.actions
+export const { setIsLoading, setMovies, sortMoviesByName, sortMoviesByScore } = moviesSlice.actions
 
 export default moviesSlice.reducer
